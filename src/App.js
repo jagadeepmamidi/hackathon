8a4e46e4ce83910e440e39e598e7e3227bf4d006
RakshithRajan <rakshith.22bce8557@vitapstudent.ac.in>
2024-09-20 17:11:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ function App() {
   return (
     <div className="App">
       <h1>MetaMask Authentication</h1>
+<<<<<<< HEAD
+      
+      <MetaMaskAuth />
+<<<<<<< Updated upstream
+      <Dashboard />
+=======
+=======
       <MetaMaskAuth />
       <Dashboard />
+>>>>>>> 3aa94f3eb84214f6e38ecabad9f4e02ac3321969
+>>>>>>> Stashed changes
       
     </div>
   );
